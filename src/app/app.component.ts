5417349d457415b8cb7d430699968a221c7454e0
sawyerbutton <lil103@pitt.edu>
2018-10-23 16:35:45 +0800

    added mouse middle button click

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-app-githubio';
+  // title = 'angular-app-githubio';
+  eventMessage = '';
+  onClick(e) {
+    e.preventDefault();
+    if ( e.which === 2 ) {
+      console.log('触发了鼠标中键');
+      this.eventMessage = '鼠标中键事件触发';
+    } else {
+      console.log('没有触发鼠标中键');
+      this.eventMessage = '鼠标中键事件未触发';
+    }
+
+  }
 }
